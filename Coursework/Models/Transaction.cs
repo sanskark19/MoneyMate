0212b1c8a3e22116a3b3f89536035683a35aa2f6
Sanskar kafle <130631579+sanskark19@users.noreply.github.com>
2025-01-14 21:58:28 +0545

    sixth commit

--- Coursework/Models/Transaction.cs
+++ Coursework/Models/Transaction.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using SQLite;
 namespace Coursework.Models;
 
@@ -5,16 +6,21 @@ public class TransactionModel
 {
     public Guid TransactionId { get; set; } = Guid.NewGuid();
     
+    [Required(ErrorMessage = "Transaction Title is required.")]
     public string Title { get; set; }
     
+    [Required(ErrorMessage = "Transaction Amount is required.")]
+    [Range(0.01, double.MaxValue, ErrorMessage = "Amount must be greater than 0")]
     public double Amount { get; set; }
     
+    [Required(ErrorMessage = "Transaction Type is required.")]
     public string TransactionType  { get; set; }
     
     public string Notes { get; set; }
     
     public string Tags { get; set; } 
     
+    [Required(ErrorMessage = "Transaction Date is required.")]
     public DateTime Date { get; set; }
     
 }
